fd3238d89b9e01e6f37ab3adb4fdb3366e3239e1
dunleavyjack <dunleavyjack@gmail.com>
2021-10-11 16:41:20 -0400

    Added refresh on pull down

--- App.tsx
+++ App.tsx
@@ -1,6 +1,45 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import {
+    RefreshControl,
+    SafeAreaView,
+    ScrollView,
+    Appearance,
+    StyleSheet,
+} from 'react-native';
 import DisplayScreen from './screens/DisplayScreen';
 
+const colorScheme = Appearance.getColorScheme();
+
+const wait = (timeout: any) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
 export default function App() {
-    return <DisplayScreen />;
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = useCallback(() => {
+        setRefreshing(true);
+        wait(1500).then(() => setRefreshing(false));
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.main}>
+            <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
+            >
+                <DisplayScreen />
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    main: {
+        backgroundColor: colorScheme === 'dark' ? 'black' : 'white',
+    },
+});
